a1883491151d9036ce879fda42d9b862dd0a1b09
05HarshaVardhan <119722670+05HarshaVardhan@users.noreply.github.com>
2023-06-10 14:47:17 +0530

    #include<stdio.h>
    int main()
    {
        int n;
        scanf("%d",&n);
        int x[n];
        int k=1;
        for(int i=0;i<n;i++)
        {
            scanf("%d",&x[i]);
            
            if(i%2!=0 && x[i]%2==0)
            {
                k=0;
            }
        }
        if(k==1)
        printf("True");
        else 
        printf("False");
        
        
    }

---
+++ Strictly_ODD.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int x[n];
+    int k=1;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+        
+        if(i%2!=0 && x[i]%2==0)
+        {
+            k=0;
+        }
+    }
+    if(k==1)
+    printf("True");
+    else 
+    printf("False");
+    
+    
+}
